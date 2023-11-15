d5cb4888392bb47ccec16683ad8902c53c44656f
BanglaDubZone <118531999+BanglaDubZone@users.noreply.github.com>
2023-11-16 00:15:52 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,15 +1,15 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 1234567890))
+    API_ID = int(env.get("TELEGRAM_API_ID", 16930870))
+    API_HASH = env.get("TELEGRAM_API_HASH", "97c229adccadb2fc1e81b4f1ce8b22e9")
+    OWNER_ID = int(env.get("OWNER_ID", 5829124764))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001234567890))
-    BOT_WORKERS = int(env.get("BOT_WORKERS", 10))
-    SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "bdzfilestreambot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6559235534:AAGjZ759ohs6V_OK__aLkNjzIsdIOqrtYZ4")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002064786619))
+    BOT_WORKERS = int(env.get("BOT_WORKERS", 15))
+    SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 9))
 
 class Server:
     BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
